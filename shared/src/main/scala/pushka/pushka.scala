23a45acc014a44dc875fa56aa51f8662d27b2a1d
Aleksey Fomkin <aleksey.fomkin@gmail.com>
2015-08-19 19:30:51 +0300

    Sealed trait support

--- shared/src/main/scala/pushka/pushka.scala
+++ shared/src/main/scala/pushka/pushka.scala
@@ -1,12 +1,9 @@
 package pushka
 
-import scala.annotation.StaticAnnotation
+import scala.annotation.{tailrec, StaticAnnotation}
 import scala.language.experimental.macros
 import scala.reflect.macros.blackbox
 
-/**
- * Thanks to https://github.com/kifi/json-annotation/
- */
 class pushka extends StaticAnnotation {
   def macroTransform(annottees: Any*): Any = macro pushkaMacro.impl
 }
@@ -17,69 +14,133 @@ class pushka extends StaticAnnotation {
 object pushkaMacro {
 
   def impl(c: blackbox.Context)(annottees: c.Expr[Any]*): c.Expr[Any] = {
+    
     import c.universe._
 
-    def caseClassRW(className: TypeName, typeParams: List[TypeDef], fields: List[ValDef]) = {
-      def rwN(n: Int) = TermName(s"rw$n")
+    def implicitRWArgDef(n: Int) = TermName(s"rw$n")
+
+    def caseClassReader(className: TypeName, fields: List[ValDef]) = {
+      
       val withIndex = fields.zipWithIndex
-      val fRWs = withIndex map { case (x, i) ⇒ q"${rwN(i)}: pushka.RW[${x.tpt}]" }
-      val fWriters = withIndex map {
-        case (x, i) ⇒ q"${x.name.toString} -> ${rwN(i)}.write(value.${x.name})"
+      val fReaders = withIndex map { 
+        case (x, i) ⇒ q"${x.name} = ${implicitRWArgDef(i)}.read(m(${x.name.toString}))" 
       }
-      val fReaders = withIndex map {
-        case (x, i) ⇒ q"${x.name} = ${rwN(i)}.read(m(${x.name.toString}))"
-      }
+      
+      q"""
+        value match {
+          case pushka.Value.Obj(m) => ${className.toTermName}(..$fReaders)
+          case _ ⇒ throw pushka.PushkaException()
+        }         
+      """
+    }
+
+    def caseClassWriter(className: TypeName, fields: List[ValDef]) = {
+      
+      val withIndex = fields.zipWithIndex
+      val fWriters = withIndex map { case (x, i) ⇒ q"${x.name.toString} -> ${implicitRWArgDef(i)}.write(value.${x.name})" }
+      
+      q"pushka.Value.Obj(Map(..$fWriters))"
+    }
+
+    def caseClassRW(className: TypeName, typeParams: List[TypeDef], fields: List[ValDef]) = {
+      
+      val withIndex = fields.zipWithIndex
+      val fRWs = withIndex map { case (x, i) ⇒ q"${implicitRWArgDef(i)}: pushka.RW[${x.tpt}]" }
+      val reader = caseClassReader(className, fields)
+      val writer = caseClassWriter(className, fields)
+      
       typeParams match {
         case Nil ⇒
           q"""
             implicit def _rw(implicit ..$fRWs): pushka.RW[$className] = new pushka.RW[$className] {
-              def read(value: pushka.Value) = value match {
-                case pushka.Value.Obj(m) => ${className.toTermName}(..$fReaders)
-                case _ ⇒ throw pushka.PushkaException()
-              }
-              def write(value: $className): pushka.Value = {
-                pushka.Value.Obj(Map(..$fWriters))
-              }
+              def read(value: pushka.Value) = $reader
+              def write(value: $className): pushka.Value = $writer
             }
           """
         case xs ⇒
+          
           val tDefs = xs.map(x ⇒ TypeDef(Modifiers(), TypeName(x.name.toString), Nil, x.rhs))
           val tParams = tDefs.map(_.name)
+          
           q"""
             implicit def _rw[..$tDefs](implicit ..$fRWs): pushka.RW[$className[..$tParams]] = new pushka.RW[$className[..$tParams]] {
-              def read(value: pushka.Value) = value match {
-                case pushka.Value.Obj(m) => ${className.toTermName}(..$fReaders)
-                case _ ⇒ throw pushka.PushkaException()
-              }
-              def write(value: $className[..$tParams]): pushka.Value = {
-                pushka.Value.Obj(Map(..$fWriters))
-              }
+              def read(value: pushka.Value) = $reader
+              def write(value: $className[..$tParams]): pushka.Value = $writer
             }
           """
       }
     }
 
     def sealedTraitRW(traitName: TypeName, compDecl: ModuleDef) = {
-      val q"object $obj extends ..$bases { ..$body }" = compDecl
-      body map {
-//        case q"case object $caseObjName extends $traitName" ⇒
-//          println("case object " + caseObjName.toString)
-        case caseClassDecl @ q"case class $className(..$fields) extends $traitName" ⇒
-          println("!!!!!!!!!!!!!!!")
-          val rw = caseClassRW(className, Nil, fields)
-          q"""
-             $caseClassDecl
-             ${modifiedCompanion(None, rw, className)}
-          """
-        case member ⇒ member
-        //        case xx ⇒ println(xx)
+      
+      @tailrec def updateBodyRec(acc: List[Tree], tail: List[Tree]): List[Tree] = tail match {
+        case Nil ⇒ acc
+        case x :: xs ⇒ x match {
+          case classDecl @ q"case class $n(..$fields) extends ..$p" if checkBases(p) ⇒
+            val newAcc = classDecl :: modifiedCompanion(None, caseClassRW(n, Nil, fields), n) :: acc
+            updateBodyRec(newAcc, xs)
+          case ignore ⇒ updateBodyRec(ignore :: acc, xs)
+        }
       }
-      val xx =
+      
+      def checkBases(xs: List[Tree]) = {
+        xs.asInstanceOf[List[Ident]].exists(_.name == traitName)
+      }
+      
+      def jsonVariantConvention(n: Name) = {
+        val s = n.toString
+        s.charAt(0).toLower + s.substring(1)
+      }
+      
+      val q"object $obj extends ..$bases { ..$body }" = compDecl
+      val updatedBody = updateBodyRec(Nil, body)
+      val names: List[Either[TermName, TypeName]] = (body: List[Tree]) collect {
+        case q"case object $n extends ..$p" if checkBases(p) ⇒ Left(n)
+        case q"case class $n(..$fields) extends ..$p" if checkBases(p) ⇒ Right(n)
+      }
+      
+      // Matching on pushka.Value to find right
+      // reader
+      val readMatcher = Match(Ident(TermName("value")), names map {
+        case Left(n) ⇒
+          CaseDef(q"pushka.Value.Str(${jsonVariantConvention(n)})", q"$n")
+        case Right(n) ⇒
+          val lower = jsonVariantConvention(n)
+          CaseDef(
+            Bind(
+              TermName("o"),
+              Typed(
+                Ident(termNames.WILDCARD),
+                TypeTree(typeOf[pushka.Value.Obj])
+              )
+            ),
+            q"o.value.contains($lower)",
+            q"${n.toTermName}._rw.read(o.value($lower))"
+          )
+      })
+      
+      // Matching on pushka.Value to find right
+      // writer
+      val writeMatcher = Match(Ident(TermName("value")), names map {
+        case Left(n) ⇒
+          CaseDef(q"$n", q"pushka.Value.Str(${jsonVariantConvention(n)})")
+        case Right(n) ⇒
+          val lower = jsonVariantConvention(n)
+          CaseDef(
+            Bind(TermName("o"), Typed(Ident(termNames.WILDCARD), Ident(n))),
+            q"pushka.Value.Obj(Map($lower -> ${n.toTermName}._rw.write(o)))"
+          )
+      })
+
       q"""
-        object ${traitName.toTermName} { ..$body }
+        object ${traitName.toTermName} { 
+          ..$updatedBody
+          implicit val _rw: RW[$traitName] = new RW[$traitName] {
+            def read(value: pushka.Value) = $readMatcher
+            def write(value: $traitName): pushka.Value = $writeMatcher
+          }
+        }
        """
-      println(show(xx))
-      xx
     }
     
     def modifiedCompanion(compDeclOpt: Option[ModuleDef], rw: Tree, className: TypeName) = {
