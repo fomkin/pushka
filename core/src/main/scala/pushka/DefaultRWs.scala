8654c9e5ced2aeb2a1307b102fa75879d32e2fd7
Aleksey Fomkin <aleksey.fomkin@gmail.com>
2015-08-20 16:44:53 +0300

    Either, Set, Map, UUID support

--- core/src/main/scala/pushka/DefaultRWs.scala
+++ core/src/main/scala/pushka/DefaultRWs.scala
@@ -1,24 +1,43 @@
 package pushka
 
+import java.util.UUID
+
 class DefaultRWs {
 
-  implicit def option[T](implicit r: Reader[T], w: Writer[T]): RW[Option[T]] = new RW[Option[T]] {
+  implicit def optionRW[T](implicit r: Reader[T], w: Writer[T]): RW[Option[T]] = new RW[Option[T]] {
     def read(value: Ast): Option[T] = value match {
       case Ast.Null ⇒ None
       case _ ⇒ Some(r.read(value))
     }
+
     def write(value: Option[T]): Ast = value match {
       case None ⇒ Ast.Null
       case Some(x) ⇒ w.write(x)
     }
   }
 
-  implicit val boolean = new RW[Boolean] {
+  implicit def eitherRW[Left, Right](implicit leftRw: RW[Left], rightRw: RW[Right]): RW[Either[Left, Right]] = {
+    new RW[Either[Left, Right]] {
+      def write(value: Either[Left, Right]): Ast = value match {
+        case Left(x) ⇒ Ast.Obj(Map("left" → leftRw.write(x)))
+        case Right(x) ⇒ Ast.Obj(Map("right" → rightRw.write(x)))
+      }
+
+      def read(value: Ast): Either[Left, Right] = value match {
+        case Ast.Obj(m) if m.contains("left") ⇒ Left(leftRw.read(m("left")))
+        case Ast.Obj(m) if m.contains("right") ⇒ Right(rightRw.read(m("right")))
+        case _ ⇒ throw PushkaException()
+      }
+    }
+  }
+
+  implicit val booleanRW = new RW[Boolean] {
     def read(value: Ast): Boolean = value match {
       case Ast.True ⇒ true
       case Ast.False ⇒ false
       case _ ⇒ throw PushkaException()
     }
+
     def write(value: Boolean): Ast = {
       if (value) Ast.True else Ast.False
     }
@@ -29,6 +48,7 @@ class DefaultRWs {
       case Ast.Num(x) ⇒ x.toInt
       case _ ⇒ throw PushkaException()
     }
+
     def write(value: Int): Ast = {
       Ast.Num(value.toDouble)
     }
@@ -39,6 +59,7 @@ class DefaultRWs {
       case Ast.Num(x) ⇒ x
       case _ ⇒ throw PushkaException()
     }
+
     def write(value: Double): Ast = {
       Ast.Num(value)
     }
@@ -49,6 +70,7 @@ class DefaultRWs {
       case Ast.Num(x) ⇒ x.toFloat
       case _ ⇒ throw PushkaException()
     }
+
     def write(value: Float): Ast = {
       Ast.Num(value.toDouble)
     }
@@ -59,6 +81,7 @@ class DefaultRWs {
       case Ast.Str(x) ⇒ x.toLong
       case _ ⇒ throw PushkaException()
     }
+
     def write(value: Long): Ast = {
       Ast.Str(value.toString)
     }
@@ -69,8 +92,18 @@ class DefaultRWs {
       case Ast.Str(x) ⇒ x
       case _ ⇒ throw PushkaException()
     }
+
     def write(value: String): Ast = {
       Ast.Str(value)
+    }
+  }
+
+  implicit val uuidRW = new RW[UUID] {
+    def write(value: UUID): Ast = Ast.Str(value.toString)
+
+    def read(value: Ast): UUID = value match {
+      case Ast.Str(s) ⇒ UUID.fromString(s)
+      case _ ⇒ throw PushkaException()
     }
   }
 
@@ -87,10 +120,26 @@ class DefaultRWs {
     }
   }
 
+  implicit def setR[T](implicit r: Reader[T]): Reader[Set[T]] = new Reader[Set[T]] {
+    def read(value: Ast): Set[T] = value match {
+      case Ast.Arr(xs) ⇒ xs.map(r.read).toSet
+      case _ ⇒ throw PushkaException()
+    }
+  }
+
   implicit def listR[T](implicit r: Reader[T]): Reader[List[T]] = new Reader[List[T]] {
     def read(value: Ast): List[T] = value match {
       case Ast.Arr(xs) ⇒ xs.map(r.read)
       case _ ⇒ throw PushkaException()
+    }
+  }
+
+  implicit def mapR[K, V](implicit r: Reader[(K, V)]): Reader[Map[K, V]] = {
+    new Reader[Map[K, V]] {
+      def read(value: Ast): Map[K, V] = value match {
+        case Ast.Arr(xs) ⇒ xs.map(r.read).toMap
+        case _ ⇒ throw PushkaException()
+      }
     }
   }
 
@@ -100,6 +149,7 @@ class DefaultRWs {
         (rw1.read(xs.head), rw2.read(xs(1)))
       case _ ⇒ throw PushkaException()
     }
+
     def write(value: (A1, A2)): Ast = {
       Ast.Arr(List(rw1.write(value._1), rw2.write(value._2)))
     }
@@ -111,6 +161,7 @@ class DefaultRWs {
         (rw1.read(xs.head), rw2.read(xs(1)), rw3.read(xs(2)))
       case _ ⇒ throw PushkaException()
     }
+
     def write(value: (A1, A2, A3)): Ast = {
       Ast.Arr(List(
         rw1.write(value._1),
--- core/src/test/scala/DefaultRWSpec.scala
+++ core/src/test/scala/DefaultRWSpec.scala
@@ -1,3 +1,5 @@
+import java.util.UUID
+
 import org.scalatest._
 import pushka._
 
@@ -10,23 +12,50 @@ class DefaultRWSpec extends FlatSpec with Matchers with TestKit {
   it should "be written as simple value" in {
     write(Option("hello")) should be(Ast.Str("hello"))
   }
-  
+
   it should "be written as null if it is empty" in {
     write(Option.empty[String]) should be(Ast.Null)
   }
-  
-  "Int" should "be read from number" in { read[Int](Ast.Num(42)) should be(42) }
-  it should "be written to number" in { write(32) should be(Ast.Num(32)) }
 
-  "Float" should "be read from number" in { read[Float](Ast.Num(42)) should be(42f) }
-  it should "be written to number" in { write(32f) should be(Ast.Num(32)) }
+  "Int" should "be read from number" in {
+    read[Int](Ast.Num(42)) should be(42)
+  }
+  it should "be written to number" in {
+    write(32) should be(Ast.Num(32))
+  }
 
-  "Double" should "be read from number" in { read[Double](Ast.Num(42)) should be(42d) }
-  it should "be written to number" in { write(32d) should be(Ast.Num(32)) }
+  "Float" should "be read from number" in {
+    read[Float](Ast.Num(42)) should be(42f)
+  }
+  it should "be written to number" in {
+    write(32f) should be(Ast.Num(32))
+  }
 
-  "Long" should "be read from string" in { read[Long](Ast.Str("42")) should be(42l) }
-  it should "be written to string" in { write(32l) should be(Ast.Str("32")) }
-  
+  "Double" should "be read from number" in {
+    read[Double](Ast.Num(42)) should be(42d)
+  }
+  it should "be written to number" in {
+    write(32d) should be(Ast.Num(32))
+  }
+
+  "Long" should "be read from string" in {
+    read[Long](Ast.Str("42")) should be(42l)
+  }
+  it should "be written to string" in {
+    write(32l) should be(Ast.Str("32"))
+  }
+
+  "UUID" should "be read from string" in {
+    read[UUID](Ast.Str("fff2a55d-afd6-44b9-ba3d-9f87685e6e50")) should be(
+      UUID.fromString("fff2a55d-afd6-44b9-ba3d-9f87685e6e50")
+    )
+  }
+  it should "be written to string" in {
+    write(UUID.fromString("fff2a55d-afd6-44b9-ba3d-9f87685e6e50")) should be(
+      Ast.Str("fff2a55d-afd6-44b9-ba3d-9f87685e6e50")
+    )
+  }
+
   "Seq" should "be read from array" in {
     val source = Ast.Arr(List(Ast.Num(1), Ast.Num(2), Ast.Num(3)))
     val pattern = Seq(1, 2, 3)
@@ -37,6 +66,60 @@ class DefaultRWSpec extends FlatSpec with Matchers with TestKit {
     val pattern = Ast.Arr(List(Ast.Num(1), Ast.Num(2), Ast.Num(3)))
     val source = Seq(1, 2, 3)
     write(source) should be(pattern)
+  }
+
+  "Map" should "be written to array of kv pairs" in {
+    val source = Map(0 → "a", 1 → "b")
+    val pattern = Ast.Arr(List(
+      Ast.Arr(List(Ast.Num(0), Ast.Str("a"))),
+      Ast.Arr(List(Ast.Num(1), Ast.Str("b")))
+    ))
+    write(source) shouldEqual pattern
+  }
+
+  it should "be read from array of pairs" in {
+    val pattern = Map(0 → "a", 1 → "b")
+    val source = Ast.Arr(List(
+      Ast.Arr(List(Ast.Num(0), Ast.Str("a"))),
+      Ast.Arr(List(Ast.Num(1), Ast.Str("b")))
+    ))
+    read[Map[Int, String]](source) shouldEqual pattern
+  }
+
+  "Set" should "be read from array" in {
+    val source = Ast.Arr(List(Ast.Num(1), Ast.Num(2), Ast.Num(3)))
+    val pattern = Set(1, 2, 3)
+    read[Set[Int]](source) should be(pattern)
+  }
+
+  it should "be written into array" in {
+    val pattern = Ast.Arr(List(Ast.Num(1), Ast.Num(2), Ast.Num(3)))
+    val source = Set(1, 2, 3)
+    write(source) should be(pattern)
+  }
+
+  "Either left" should "be read from object" in {
+    val source = Ast.Obj(Map("left" → Ast.Str("hello")))
+    val pattern = Left("hello")
+    read[Either[String, Int]](source) should be(pattern)
+  }
+
+  it should "be written into object" in {
+    val source = Left("hello")
+    val pattern = Ast.Obj(Map("left" → Ast.Str("hello")))
+    write[Either[String, Int]](source) should be(pattern)
+  }
+
+  "Either right" should "be read from object" in {
+    val source = Ast.Obj(Map("right" → Ast.Num(20)))
+    val pattern = Right(20)
+    read[Either[String, Int]](source) should be(pattern)
+  }
+
+  it should "be written into object" in {
+    val source = Right(20)
+    val pattern = Ast.Obj(Map("right" → Ast.Num(20)))
+    write[Either[String, Int]](source) should be(pattern)
   }
 
 }
